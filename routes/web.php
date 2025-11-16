2dd98458744a47cf1f1f45e8d69c41f04c545181
dimitris georgiadis <geojimas@gmail.com>
2025-11-16 02:07:20 +0200

    serve the client static files from laravel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get(
+    '/{any}',
+    fn () => file_get_contents(public_path('index.html'))
+)->where('any', '.*');
