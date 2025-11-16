1b69fbc32ce0bbfba3e1e4549f41bf6856d9ce42
dimitris georgiadis <geojimas@gmail.com>
2025-11-16 02:03:46 +0200

    create frontend code

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { Button } from 'primereact/button';                             
+import { Card } from 'primereact/card';
+
+function App() {
+
+  return (
+    <div>
+      <Card title="Simple Card">
+    <p className="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+        numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+    </p>
+</Card>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { PrimeReactProvider } from 'primereact/api';
+import App from './App.jsx'
+import "primereact/resources/themes/mdc-light-indigo/theme.css";
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+  </StrictMode>,
+)
