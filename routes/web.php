b3f154edbe9657c4c88e20e9f6e025ce6cf5c5f5
Dimitris Georgiadis <geojimas@gmail.com>
2025-12-26 19:33:55 +0200

    make caddy work with project

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// Serve SPA routes (everything except files)
-Route::get('/{any}', function () {
-    return file_get_contents(public_path('dist/index.html'));
-})->where('any', '.*');
